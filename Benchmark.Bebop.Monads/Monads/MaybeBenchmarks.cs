943efdf0d9d9f5d2d1bdf123b85f9a8e2febd5f2
Alexander Jesner <alexander@jesner.eu>
2019-06-29 22:02:50 +0200

    Add benchmark project

---
+++ Benchmark.Bebop.Monads/Monads/MaybeBenchmarks.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2018, Alexander Jesner
+// License: https://opensource.org/licenses/MIT
+
+using BenchmarkDotNet.Attributes;
+using System;
+
+namespace Bebop.Monads
+{
+    [ClrJob(baseline: true)]
+    [RPlotExporter, RankColumn]
+    [DisassemblyDiagnoser(printAsm: true, printSource: true)]
+    public class MaybeBenchmarks
+    {
+        [Params(500)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+        }
+        
+        [Benchmark]
+        public Maybe<int> GenericValueTypeFrom() => Maybe.From(123);
+
+        [Benchmark]
+        public Maybe<int> GenericValueTypeNothing() => Maybe.Nothing<int>();
+
+        [Benchmark]
+        public Maybe<string> GenericReferenceTypeFrom() => Maybe.From("asdf");
+
+        [Benchmark]
+        public Maybe<string> GenericReferenceTypeNothing() => Maybe.Nothing<string>();
+        
+        [Benchmark]
+        public IMaybe NonGenericValueTypeFrom() => Maybe.From(typeof(int), 123);
+
+        [Benchmark]
+        public IMaybe NonGenericValueTypeNothing() => Maybe.Nothing(typeof(int));
+
+        [Benchmark]
+        public IMaybe NonGenericReferenceTypeFrom() => Maybe.From(typeof(string), "asdf");
+
+        [Benchmark]
+        public IMaybe NonGenericReferenceTypeNothing() => Maybe.Nothing(typeof(string));
+        
+        [Benchmark]
+        public IMaybe NonGenericCastableValueTypeFrom() => Maybe.Castable.From(typeof(int), 123);
+
+        [Benchmark]
+        public IMaybe NonGenericCastableValueTypeNothing() => Maybe.Castable.Nothing(typeof(int));
+
+        [Benchmark]
+        public IMaybe NonGenericCastableReferenceTypeFrom() => Maybe.Castable.From(typeof(string), "asdf");
+
+        [Benchmark]
+        public IMaybe NonGenericCastableReferenceTypeNothing() => Maybe.Castable.Nothing(typeof(string));
+    }
+}
---
+++ Benchmark.Bebop.Monads/Monads/Program.cs
@@ -0,0 +1,15 @@
+﻿// Copyright 2018, Alexander Jesner
+// License: https://opensource.org/licenses/MIT
+
+using BenchmarkDotNet.Running;
+
+namespace Bebop.Monads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MaybeBenchmarks>();
+        }
+    }
+}
